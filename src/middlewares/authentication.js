bf558bc2288b3ae2a286f95e5d8b3731572803d7
ilkay <06ilkayozturk@gmail.com>
2023-11-25 12:32:50 +0300

    feat:project is folder created

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,9 @@
+"use strict";
+/* --------------------------------------
+    NODEJS EXPRESS | Stock Management API
+----------------------------------------- */
+// app.use(authentication):
+
+module.exports = async (req, res, next) => {
+  next();
+};
---
+++ src/models/purchase.js
@@ -0,0 +1,6 @@
+"use strict";
+/* --------------------------------------
+    NODEJS EXPRESS | Stock Management API
+----------------------------------------- */
+const { mongoose } = require("../configs/dbConnection");
+/* ------------------------------------------------------- */
