cb0d39ad11b902a3a5c182aba68cca9190a706a9
ilkay <06ilkayozturk@gmail.com>
2023-11-28 13:18:41 +0300

    synchronized

--- index.js
+++ index.js
@@ -71,4 +71,4 @@ app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
-// require("./src/helpers/sync")(); // !!! It clear database.
+// require("./src/helpers/sync")(); // !!! It clear database.Boom
