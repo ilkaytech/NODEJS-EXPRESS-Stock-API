0a1e7794090aa8e50d01722b0a9a0e46d9b41e2f
ilkay <06ilkayozturk@gmail.com>
2023-11-30 15:19:21 +0300

    fix:sync

--- src/helpers/sync.js
+++ src/helpers/sync.js
@@ -8,10 +8,10 @@ module.exports = async function () {
   // return null;
 
   /* REMOVE DATABASE *
-    const { mongoose } = require('../configs/dbConnection')
-    await mongoose.connection.dropDatabase()
-    console.log('- Database and all data DELETED!')
-    /* REMOVE DATABASE */
+  const { mongoose } = require('../configs/dbConnection')
+  await mongoose.connection.dropDatabase()
+  console.log('- Database and all data DELETED!')
+  /* REMOVE DATABASE */
 
   /* User */
   const User = require("../models/user");
